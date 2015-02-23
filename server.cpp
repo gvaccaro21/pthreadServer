d3d2ff5b499d7bcceb5c94e12e0a90211a26cdd3
Joseph Alexander Gibbs <jgibbs01.student@snoopy.rlc.manhattan.edu>
2015-02-23 17:08:58 -0500

    Update server to output instructions, enable mutex from command line (disabled by default)

--- server.cpp
+++ server.cpp
@@ -30,26 +30,40 @@ void error(const char *msg)
     exit(1);
 }
 
+typedef struct {
+	bool useMutex;
+	int socketNum;
+} sockMutex;
+
 void *multiply(void* threadarg) {
 	stringstream ss;
 	char buffer[256];
 	int n;
 	double numArray[2];
      	double product;
-	int socketNum = *((int*)threadarg);
-
-	pthread_mutex_lock(&updateMutex);
-	connectionCount++;
-	pthread_mutex_unlock(&updateMutex);		
+	sockMutex sockInfo = *((sockMutex*)threadarg);
+	int socketNum = sockInfo.socketNum;
+	
+	if (sockInfo.useMutex) {
+		pthread_mutex_lock(&updateMutex);
+		connectionCount++;
+		pthread_mutex_unlock(&updateMutex);
+	}
+	else
+		connectionCount++;
 	bzero(buffer,256);
      	n = read(socketNum,buffer,255);
      	if (n < 0) error("ERROR reading from socket");
 	sscanf(buffer, "%lf%lf", &numArray[0], &numArray[1]);
      	printf("Here is the message: %s\n",buffer);
      	product = numArray[0] * numArray[1];
-	pthread_mutex_lock(&productMutex);
-	productSum += product;
-	pthread_mutex_unlock(&productMutex);
+	if (sockInfo.useMutex) {
+		pthread_mutex_lock(&productMutex);
+		productSum += product;
+		pthread_mutex_unlock(&productMutex);
+	}
+	else
+		productSum += product;
      	ss << "The product is " <<  product;
      	string prodstring = ss.str();
 	ss.str("");
@@ -65,32 +79,48 @@ int main(int argc, char *argv[])
      socklen_t clilen;
      long t;
      void *status;
-     
+     bool useMutex = false;
+
      pthread_t socketThread[THREAD_COUNT];
      pthread_attr_t attr;
-     
-    
+
      struct sockaddr_in serv_addr, cli_addr;
      int i, rc;
-     if (argc < 2) {
-         fprintf(stderr,"ERROR, no port provided\n");
+     if (argc < 3) {
+         fprintf(stderr,"Usage:\n%s -p port_number -m\n\t-p, --port: Port number (anything above 1024)\n\t-m, --mutex: Enable mutex\n", argv[0]);
          exit(1);
      }
      sockfd = socket(AF_INET, SOCK_STREAM, 0);
      if (sockfd < 0) 
         error("ERROR opening socket");
      bzero((char *) &serv_addr, sizeof(serv_addr));
-     portno = atoi(argv[1]);
+      /* check command line arguments for fixed seed flag */
+        for (int i=0; i<argc; i++) {
+                if (strstr(argv[i], "-p") || strstr(argv[i], "--port")) {
+                        portno = atoi(argv[i+1]);
+                        continue;
+                }
+		else if (strstr(argv[i], "-m") || strstr(argv[i], "--mutex")) {
+			useMutex = true;
+			continue;
+        	}
+	}
      serv_addr.sin_family = AF_INET;
      serv_addr.sin_addr.s_addr = INADDR_ANY;
      serv_addr.sin_port = htons(portno);
      if (bind(sockfd, (struct sockaddr *) &serv_addr,
               sizeof(serv_addr)) < 0) 
               error("ERROR on binding");
+	printf("Running server on port %d\n", portno);
+	if (useMutex) {
+		printf("Running mutexes\n");
+	}
      pthread_attr_init(&attr);
      pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
 //     while (1) 
      for (i = 0; i <= THREAD_COUNT; i++) {
+	sockMutex socketInfo;
+	socketInfo.useMutex = useMutex;
 	listen(sockfd,5);
     	clilen = sizeof(cli_addr);
      	newsockfd = accept(sockfd, 
@@ -99,7 +129,8 @@ int main(int argc, char *argv[])
      	if (newsockfd < 0) 
         	  error("ERROR on accept");
 //	int *socketNum = malloc(sizeof(*socketNum));
-	pthread_create(&socketThread[i], &attr, multiply, (void *) &newsockfd);
+	socketInfo.socketNum = newsockfd;
+	pthread_create(&socketThread[i], &attr, multiply, (void *) &socketInfo);
      }
  
      pthread_attr_destroy(&attr);
